1d55c102d9027b94c683856ff3a7593f9bff949c
WJL <52209658+wjl-Learner@users.noreply.github.com>
2019-10-30 18:29:13 +0800

    push

--- js/index.js
+++ js/index.js
@@ -1,23 +1,28 @@
-$(function(){
+$(function () {
     $('.container').fullpage({
-        navigation: true
+        navigation: true,
+        navigationPosition: 'right',
+        anchors: ["page1", "page2", "page3", "page4", "page5"], //注意不带#
+        menu: "#menu", //绑定菜单，data-menuanchor,才能使用.active
+        afterLoad: function (link, index) {
+            if (index.index === 2) {
+                $(".center").fadeIn(1000)
+            }
+            if (index.index === 1) {
+                $(".info").fadeIn(1000)
+                $(".info-text").fadeIn(1000)
+            }
+        },
+        
     });
 })
 
-$(document).ready(function(){
-    $(".logo").mouseover(function(){
-      $(".like").show();
+$(document).ready(function () {
+    $(".logo").mouseover(function () {
+        $(".like").show();
     });
-    $(".logo").mouseout(function(){
+    $(".logo").mouseout(function () {
         $(".like").hide();
-      });
-  });
-  
-function toggleNav() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+    });
+
+});
